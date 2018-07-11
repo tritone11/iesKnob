e94ed6eac148d110444884363a8ea2d29f4cdbdc
Tritone <tritone11@users.noreply.github.com>
2018-07-11 12:11:57 +0200

    Added icon on hover

--- source.js
+++ source.js
@@ -1,173 +1,66 @@
 window._iesKnob = {
-  att: function( e, a, v ) {
-    if ( e ) {
-      if ( v ) {
-        e.setAttribute( a, v );
-      } else {
-        return e.getAttribute( a )
-      }
-    }
-    return false
-  },
-  $$$: function( e ) {
-    if ( typeof e == 'string' ) {
-      e = document.getElementById( e );
-    }
-    return e
-  },
-  ins: function( p, tag, aa, _html, b, ns ) {
-    var i;
-    if ( ns ) {
-      var elm = document.createElementNS( "http://www.w3.org/2000/svg", tag );
-    } else {
-      var elm = document.createElement( tag );
-    }
-    if ( _html ) {
-      elm.innerHTML = _html;
-    }
-    p = _iesKnob.$$$( p );
-    if ( aa ) {
-      for ( i = 0; i < aa.length; i += 2 ) {
-        this.att( elm, aa[ i ], aa[ i + 1 ] );
-      }
-    }
-    if ( p ) {
-      if ( b == true ) {
-        return p.insertBefore( elm, p.firstChild );
-      } else if ( b ) {
-        return p.insertBefore( elm, b );
-      } else {
-        return p.appendChild( elm );
-      }
-    } else {
-      return elm
-    }
-  },
-  init: function( force ) {
-    var st = document.createElement( "style" );
-    if ( document.getElementById( "circle-animation-style" ) ) {
-      if ( force ) {
-        document.getElementById( "circle-animation-style" ).innerHTML = ""
-      }
-    } else {
-      st.id = "circle-animation-style";
-      document.head.appendChild( st );
-    }
-    var knobs = document.getElementsByClassName( "iesknob" );
-    for ( var i = 0; i < knobs.length; i++ ) {
-      if ( knobs[ i ].getAttribute( "initiated" ) != "true" || force == "force" ) {
-        if ( knobs[ i ].id == "" ) {
-          knobs[ i ].id = _iesKnob.guid();
+    att: function(t, e, i) {
+        if (t) {
+            if (!i) return t.getAttribute(e);
+            t.setAttribute(e, i)
         }
-        _iesKnob.generate( knobs[ i ].id, knobs[ i ].getAttribute( "data-knob-image" ), knobs[ i ].getAttribute( "data-knob-dotimage" ), knobs[ i ].getAttribute( "data-knob-percentage" ), knobs[
-          i ].getAttribute( "data-knob-timing" ), knobs[ i ].getAttribute( "data-knob-dotcolor" ), knobs[ i ].getAttribute( "data-knob-gradient1" ), knobs[ i ].getAttribute(
-          "data-knob-gradient2" ), knobs[ i ].getAttribute( "data-knob-color" ), knobs[ i ].getAttribute( "data-knob-dotcolorinside" ), knobs[ i ].getAttribute( "data-knob-dotborder" ), knobs[
-          i ].getAttribute( "data-knob-dotradius" ) );
-        knobs[ i ].setAttribute( "initiated", "true" );
-      }
+        return !1
+    },
+    $$$: function(t) {
+        return "string" == typeof t && (t = document.getElementById(t)), t
+    },
+    ins: function(t, e, i, r, n, o) {
+        var a;
+        if (o) var s = document.createElementNS("http://www.w3.org/2000/svg", e);
+        else s = document.createElement(e);
+        if (r && (s.innerHTML = r), t = _iesKnob.$$$(t), i)
+            for (a = 0; a < i.length; a += 2) this.att(s, i[a], i[a + 1]);
+        return t ? 1 == n ? t.insertBefore(s, t.firstChild) : n ? t.insertBefore(s, n) : t.appendChild(s) : s
+    },
+    init: function(t) {
+        var e = document.createElement("style");
+        document.getElementById("circle-animation-style") ? t && (document.getElementById("circle-animation-style").innerHTML = "") : (e.id = "circle-animation-style", document.head.appendChild(e));
+        for (var i = document.getElementsByClassName("iesknob"), r = 0; r < i.length; r++) "true" == i[r].getAttribute("initiated") && "force" != t || ("" == i[r].id && (i[r].id = _iesKnob.guid()), _iesKnob.generate(i[r].id, i[r].getAttribute("data-knob-image"), i[r].getAttribute("data-knob-dotimage"), i[r].getAttribute("data-knob-percentage"), i[r].getAttribute("data-knob-timing"), i[r].getAttribute("data-knob-dotcolor"), i[r].getAttribute("data-knob-gradient1"), i[r].getAttribute("data-knob-gradient2"), i[r].getAttribute("data-knob-color"), i[r].getAttribute("data-knob-dotcolorinside"), i[r].getAttribute("data-knob-dotborder"), i[r].getAttribute("data-knob-dotradius"), i[r].getAttribute("data-knob-strokewidth"), i[r].getAttribute("data-knob-html"), i[r].getAttribute("data-knob-colorhover"), i[r].getAttribute("data-knob-iconhover"), i[r].getAttribute("data-knob-iconsize")), i[r].setAttribute("initiated", "true"))
+    },
+    guid: function() {
+        for (var t = "", e = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz", i = 0; i < 15; i++) t += e.charAt(Math.floor(Math.random() * e.length));
+        return t
+    },
+    backgrounds: function(t, e, i) {
+        if (t.getAttribute("data-oldstyle")) var r = t.getAttribute("data-oldstyle");
+        else r = t.getAttribute("style");
+        t.setAttribute("data-oldstyle", r), e = e + ";background-size: 99% ," + i + ";background-position: 50% 50%;background-repeat: no-repeat", t.setAttribute("style", r + ";background:" + e)
+    },
+    generate: function(t, e, i, r, n, o, a, s, l, d, c, u, g, b, m, f, h) {
+        var k = -1 < navigator.userAgent.indexOf("Safari"),
+            y = -1 < window.navigator.userAgent.indexOf("Edge"); - 1 < navigator.userAgent.indexOf("Chrome") && k && (k = !1);
+        var p = document.getElementById(t);
+        p.innerHTML = "";
+        var v = _iesKnob.guid(),
+            A = _iesKnob.ins(p, "svg", ["class", "circle-chart", "id", v, "height", "103%", "width", "103%", "viewbox", "0 0 33.83098862 33.83098862", "xmlns", "http://www.w3.org/2000/svg", "style", "height:103%;width:103%;overflow: visible  !important;box-shadow : 0px -0px 10000px transparent;position:absolute;top:-1.5%;left:-1.5%", "preserveAspectRatio", "none"], !1, !1, !0),
+            _ = _iesKnob.ins(A, "defs", !1, !1, !1, !0),
+            w = _iesKnob.guid(),
+            K = _iesKnob.ins(_, "linearGradient", ["id", w, "x1", "0", "y1", "10", "x2", "30", "y2", "10", "gradientUnits", "userSpaceOnUse", "gradientTransform", "rotate(90)"], !1, !1, !0);
+        if (a && null != a && "" != a || (a = "#FFD700"), s && null != s && "" != s || (s = "yellow"), _iesKnob.ins(K, "stop", ["stop-color", a, "offset", "0"], !1, !1, !0), _iesKnob.ins(K, "stop", ["stop-color", s, "offset", "1"], !1, !1, !0), b) {
+            var x = document.createElement("div");
+            x.setAttribute("style", "position:absolute;top:0;left:0;width:100%;height:100%;display: table; overflow:hidden;border-radius:50%");
+            var E = document.createElement("div");
+            E.setAttribute("style", "display: table-cell; vertical-align: middle;text-align: center;"), E.innerHTML = e, x.appendChild(E), p.appendChild(x)
+        } else p.style.background = "url(" + e + ")", m && f && (h || (h = "99%"), p.setAttribute("onmouseover", "_iesKnob.backgrounds(this,'linear-gradient(0deg," + m + "," + m + ") , url(" + f + ") , url(" + e + ") ','" + h + "')"), p.setAttribute("onmouseleave", "_iesKnob.backgrounds(this,'url(" + e + ") ','" + h + "')"));
+        if (p.style.backgroundSize = "99%", p.style.backgroundPosition = "50% 50%", p.style.backgroundRepeat = "no-repeat", g && null != g && "" != g || (g = "2"), l && null != l && "" != l || (l = "#efefef"), _iesKnob.ins(A, "circle", ["class", "circle-chart__background", "stroke", l, "stroke-width", g, "fill", "none", "cx", "16.91549431", "cy", "16.91549431", "r", "15.91549431"], !1, !1, !0), y ? _iesKnob.ins(A, "circle", ["class", "circle-chart__circle", "stroke", "url(#" + w + ")", "stroke-width", g, "stroke-dasharray", r + ",100", "stroke-linecap", "round", "fill", "none", "cx", "16.91549431", "cy", "16.91549431", "r", "15.91549431", "transform", "translate(-0,33.83098862) rotate(-90)"], !1, !1, !0) : _iesKnob.ins(A, "circle", ["class", "circle-chart__circle", "stroke", "url(#" + w + ")", "stroke-width", g, "stroke-dasharray", r + ",100", "stroke-linecap", "round", "fill", "none", "cx", "16.91549431", "cy", "16.91549431", "r", "15.91549431"], !1, !1, !0), i)
+            if (y || k) {
+                var B = _iesKnob.ins(p, "div", ["style", "position:absolute;top:0%;left:42.5%;width:15%;height:50%", "class", "star-element"]);
+                _iesKnob.ins(B, "img", ["src", i, "style", "position:absolute;top:-15%;left:0%;width:100%;"])
+            } else _iesKnob.ins(A, "image", ["href", i, "height", "6", "width", "6", "class", "star-element", "x", "14", "y", "-2"], !1, !1, !0);
+        else {
+            o && null != o && "" != o || (o = "#000000"), d && null != d && "" != d || (d = o), c && null != c && "" != c || (c = "0.1"), u && null != u && "" != u || (u = "0.9");
+            var M = _iesKnob.ins(A, "g", ["class", "star-element", "style", "position:relative"], !1, !1, !0);
+            _iesKnob.ins(M, "circle", ["stroke", o, "stroke-width", c, "r", u, "fill", d, "cx", "17", "cy", "1.1"], !1, !1, !0)
+        }
+        p.innerHTML = p.innerHTML, document.getElementById(v).classList.add(v);
+        var L = 360 / (100 / r);
+        if (n && null != n && "" != n || (n = 1), y || k) var T = "@keyframes " + v + "-circle-chart-fill {to { stroke-dasharray: 0 100; }}@keyframes " + v + "-rotate-star {to { transform: rotate(0deg) }}." + v + " .circle-chart__circle {animation-timing-function: ease-out;animation: " + v + "-circle-chart-fill " + n + "s reverse;transform: rotate(-90deg);transform-origin: center}." + v + " + .star-element {animation-timing-function: ease-out;animation: " + v + "-rotate-star " + n + "s reverse;transform: rotate(" + L + "deg);transform-origin: bottom;}";
+        else T = "@keyframes " + v + "-circle-chart-fill {to { stroke-dasharray: 0 100; }}@keyframes " + v + "-rotate-star {to { transform: rotate(0deg) }}." + v + " .circle-chart__circle {animation-timing-function: ease-out;animation: " + v + "-circle-chart-fill " + n + "s reverse;transform: rotate(-90deg);transform-origin: center}." + v + " .star-element {animation-timing-function: ease-out;animation: " + v + "-rotate-star " + n + "s reverse;transform: rotate(" + L + "deg);transform-origin: center 50%;}";
+        y && (T += "*{-webkit-backface-visibility: hidden;}"), document.getElementById("circle-animation-style").innerHTML += T
     }
-  },
-  guid: function() {
-    var anitext = "";
-    var anipossible = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz";
-    for ( var i = 0; i < 15; i++ ) {
-      anitext += anipossible.charAt( Math.floor( Math.random() * anipossible.length ) );
-    };
-    return anitext;
-  },
-  generate: function( targetId, userImg, starImg, percentage, timing, dotcolor, gcol1, gcol2, bkgcol, dotinside, dotborder, dotradius ) {
-    var safari = navigator.userAgent.indexOf( "Safari" ) > -1;;
-    var edge = window.navigator.userAgent.indexOf( "Edge" ) > -1;
-    var is_chrome = navigator.userAgent.indexOf( 'Chrome' ) > -1;
-    if ( ( is_chrome ) && ( safari ) ) {
-      safari = false;
-    }
-    var target = document.getElementById( targetId );
-    target.innerHTML = "";
-    var tgid = _iesKnob.guid();
-    var tg = _iesKnob.ins( target, 'svg', [ "class", "circle-chart", "id", tgid, "height", "102%", "width", "102%", "viewbox", "0 0 33.83098862 33.83098862", "xmlns", "http://www.w3.org/2000/svg",
-      "style", "height:102%;width:102%;overflow: visible  !important;box-shadow : 0px -0px 10000px transparent;position:absolute;top:-1%;left:-1%", "preserveAspectRatio", "none"
-    ], false, false, true );
-    var defs = _iesKnob.ins( tg, 'defs', false, false, false, true );
-    var gradientId = _iesKnob.guid();
-    var lin = _iesKnob.ins( defs, "linearGradient", [ "id", gradientId, "x1", "0", "y1", "10", "x2", "30", "y2", "10", "gradientUnits", "userSpaceOnUse", "gradientTransform", "rotate(90)" ],
-      false, false, true );
-    if ( !gcol1 || gcol1 == null || gcol1 == "" ) {
-      gcol1 = "#FFD700"
-    };
-    if ( !gcol2 || gcol2 == null || gcol2 == "" ) {
-      gcol2 = "yellow"
-    };
-    _iesKnob.ins( lin, "stop", [ "stop-color", gcol1, "offset", "0" ], false, false, true );
-    _iesKnob.ins( lin, "stop", [ "stop-color", gcol2, "offset", "1" ], false, false, true );
-    //var rectId=_iesKnob.guid();
-    //_iesKnob.ins(defs,"rect",["id",rectId,"x","5%","y","5%","width","92%","height","92%","rx","15"],false,false,true);
-    //var clipId=_iesKnob.guid();
-    //var clip=_iesKnob.ins(defs,"clipPath",["id",clipId],false,false,true);
-    //_iesKnob.ins(clip,"use",["xlink:href","#"+rectId],false,false,true);
-    //_iesKnob.ins(tg,"image",["href",userImg,"height","34","width","34","clip-path","url(#"+clipId+")"],false,false,true);
-    target.style.backgroundImage = "url(" + userImg + ")";
-    target.style.backgroundSize = "99%";
-    target.style.backgroundPosition = "50% 50%";
-    target.style.backgroundRepeat = "no-repeat";
-    if ( !bkgcol || bkgcol == null || bkgcol == "" ) {
-      bkgcol = "#efefef"
-    }
-    _iesKnob.ins( tg, "circle", [ "class", "circle-chart__background", "stroke", bkgcol, "stroke-width", "2", "fill", "none", "cx", "16.91549431", "cy", "16.91549431", "r", "15.91549431" ], false,
-      false, true );
-    if ( edge ) {
-      _iesKnob.ins( tg, "circle", [ "class", "circle-chart__circle", "stroke", "url(#" + gradientId + ")", "stroke-width", "2", "stroke-dasharray", percentage + ",100", "stroke-linecap", "round",
-        "fill", "none", "cx", "16.91549431", "cy", "16.91549431", "r", "15.91549431", "transform", "translate(-0,33.83098862) rotate(-90)"
-      ], false, false, true );
-    } else {
-      _iesKnob.ins( tg, "circle", [ "class", "circle-chart__circle", "stroke", "url(#" + gradientId + ")", "stroke-width", "2", "stroke-dasharray", percentage + ",100", "stroke-linecap", "round",
-        "fill", "none", "cx", "16.91549431", "cy", "16.91549431", "r", "15.91549431"
-      ], false, false, true );
-    }
-    if ( starImg ) {
-      //_iesKnob.ins(tg,"image",["href",starImg,"height","6","width","6","class","star-element","x","14","y","-2"],false,false,true);
-      if ( edge || safari ) {
-        var imgdiv = _iesKnob.ins( target, "div", [ "style", "position:absolute;top:0%;left:42.5%;width:15%;height:50%", "class", "star-element" ] )
-        _iesKnob.ins( imgdiv, "img", [ "src", starImg, "style", "position:absolute;top:-15%;left:0%;width:100%;" ] );
-      } else {
-        _iesKnob.ins( tg, "image", [ "href", starImg, "height", "6", "width", "6", "class", "star-element", "x", "14", "y", "-2" ], false, false, true );
-      }
-    } else {
-      if ( !dotcolor || dotcolor == null || dotcolor == "" ) {
-        dotcolor = "#000000"
-      };
-      if ( !dotinside || dotinside == null || dotinside == "" ) {
-        dotinside = dotcolor
-      };
-      if ( !dotborder || dotborder == null || dotborder == "" ) {
-        dotborder = "0.1"
-      };
-      if ( !dotradius || dotradius == null || dotradius == "" ) {
-        dotradius = "0.9"
-      };
-      var g = _iesKnob.ins( tg, "g", [ "class", "star-element", "style", "position:relative" ], false, false, true );
-      _iesKnob.ins( g, "circle", [ "stroke", dotcolor, "stroke-width", dotborder, "r", dotradius, "fill", dotinside, "cx", "17", "cy", "1.1" ], false, false, true );
-    }
-    target.innerHTML = target.innerHTML;
-    var nTg = document.getElementById( tgid );
-    nTg.classList.add( tgid );
-    var percentageDeg = 360 / ( 100 / percentage );
-    if ( !timing || timing == null || timing == "" ) {
-      timing = 1
-    };
-    if ( edge || safari ) {
-      var style = "@keyframes " + tgid + "-circle-chart-fill {to { stroke-dasharray: 0 100; }}@keyframes " + tgid + "-rotate-star {to { transform: rotate(0deg) }}." + tgid +
-        " .circle-chart__circle {animation-timing-function: ease-out;animation: " + tgid + "-circle-chart-fill " + timing + "s reverse;transform: rotate(-90deg);transform-origin: center}." + tgid +
-        " + .star-element {animation-timing-function: ease-out;animation: " + tgid + "-rotate-star " + timing + "s reverse;transform: rotate(" + percentageDeg + "deg);transform-origin: bottom;}"
-    } else {
-      var style = "@keyframes " + tgid + "-circle-chart-fill {to { stroke-dasharray: 0 100; }}@keyframes " + tgid + "-rotate-star {to { transform: rotate(0deg) }}." + tgid +
-        " .circle-chart__circle {animation-timing-function: ease-out;animation: " + tgid + "-circle-chart-fill " + timing + "s reverse;transform: rotate(-90deg);transform-origin: center}." + tgid +
-        " .star-element {animation-timing-function: ease-out;animation: " + tgid + "-rotate-star " + timing + "s reverse;transform: rotate(" + percentageDeg +
-        "deg);transform-origin: center 50%;}"
-    }
-    if ( edge ) {
-      style = style + "*{-webkit-backface-visibility: hidden;}";
-    }
-    document.getElementById( "circle-animation-style" ).innerHTML += style;
-  }
-}
+}, _iesKnob.init("force");
